766be18b28d08011880483006371acb978f6271c
Leeuc <92780253+Leewuc@users.noreply.github.com>
2024-01-25 18:38:16 +0900

    Title: 369 게임, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240125/369 게임/369-game.py
@@ -0,0 +1,12 @@
+def play_369_game(n):
+    results = []
+    for number in range(1, n+1):
+        if number % 3 == 0 or '3' in str(number) or '6' in str(number) or '9' in str(number):
+            results.append('0')
+        else:
+            results.append(str(number))
+    return ' '.join(results)
+
+n = 3
+game_results = play_369_game(n)
+print(game_results,end=" ")
