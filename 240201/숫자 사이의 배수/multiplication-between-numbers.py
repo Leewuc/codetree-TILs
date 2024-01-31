7bbd325f763bb06a961579dd49f78f858ff39f12
Leeuc <92780253+Leewuc@users.noreply.github.com>
2024-02-01 03:53:18 +0900

    Title: 숫자 사이의 배수, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240201/숫자 사이의 배수/multiplication-between-numbers.py
@@ -0,0 +1,10 @@
+a,b = map(int,input().split())
+sum = 0
+mean = 0
+cnt = 0
+for i in range(a,b+1):
+    if(i % 5 == 0 or i  % 7 == 0):
+        sum += i
+        cnt += 1
+        mean = round(sum / cnt,1)
+print(sum, mean)
