b15128ac6af8eb1e0582a1fb8a069d435d81cd50
Leeuc <92780253+Leewuc@users.noreply.github.com>
2024-03-29 17:30:27 +0900

    Title: 소수 판별기, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240329/소수 판별기/prime-number-discriminator.py
+++ 240329/소수 판별기/prime-number-discriminator.py
@@ -1,9 +1,9 @@
 n = int(input())
 bb = False
-for i in range(1,n+1):
-    if n % i != 0:
+for i in range(2,n):
+    if n % i == 0:
         bb = True
 if bb == True:
-    print("P")
-else:
     print("C")
+else:
+    print("P")
