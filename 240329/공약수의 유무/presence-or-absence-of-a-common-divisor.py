5738412dd865836751461a156e9806c4261b0a90
Leeuc <92780253+Leewuc@users.noreply.github.com>
2024-03-29 15:50:58 +0900

    Title: 공약수의 유무, Time: 158ms, Memory: 24MB, Status: Passed - CodeTree

--- 240329/공약수의 유무/presence-or-absence-of-a-common-divisor.py
+++ 240329/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -1,9 +1,9 @@
 a,b = map(int,input().split())
-b = False
+bb = False
 for i in range(a,b+1):
     if (1920%i)==0 and (2880%i) == 0:
-        b = True
-if b == True:
+        bb = True
+if bb == True:
     print(1)
 else:
     print(0)
