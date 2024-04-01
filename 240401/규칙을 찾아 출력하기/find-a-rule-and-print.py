2527259a3c7144b30bda876f5f4859d621b434a8
Leeuc <92780253+Leewuc@users.noreply.github.com>
2024-04-01 15:36:33 +0900

    Title: 규칙을 찾아 출력하기, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240401/규칙을 찾아 출력하기/find-a-rule-and-print.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+for i in range(n):
+    if i == 0 or i == n-1:
+        print("* " * n)
+    else:
+        for j in range(n):
+            if i > j or j == n-1:
+                print("*", end = " ")
+            elif i <= j:
+                print(" ", end = " ")
+        print()
