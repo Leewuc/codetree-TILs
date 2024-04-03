818b0e2d8faf26fe0f353eb48689842c31c6db15
Leeuc <92780253+Leewuc@users.noreply.github.com>
2024-04-03 15:50:21 +0900

    Title: 100 도달하기, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240403/100 도달하기/reach-100.py
+++ 240403/100 도달하기/reach-100.py
@@ -4,11 +4,10 @@ arr = []
 arr.append(1)
 arr.append(n)
 
-for i in range(2,10):
+for i in range(2,13):
     arr.append(arr[i-1]+arr[i-2])
 
 for elem in arr:
-    
     print(elem,end=" ")
     if elem > 100:
         break
