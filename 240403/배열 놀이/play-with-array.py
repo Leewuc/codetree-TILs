69d67cd0a1f6ed449d6da35d2934de4382e90048
Leeuc <92780253+Leewuc@users.noreply.github.com>
2024-04-03 17:30:59 +0900

    Title: 배열 놀이, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240403/배열 놀이/play-with-array.py
+++ 240403/배열 놀이/play-with-array.py
@@ -4,12 +4,12 @@ elements = list(map(int, input().split()))
 for _ in range(q):
     query = input().split()
     if query[0] == '1':
-        idx = int(query[1]) - 1
+        idx = int(query[1])-1
         print(elements[idx])
     elif query[0] == '2':
         value = int(query[1])
         idx = next((i for i, x in enumerate(elements) if x == value), 0)
-        print(idx + 1)
+        print(idx+1)
     elif query[0] == '3':
         start = int(query[1]) - 1
         end = int(query[2])
