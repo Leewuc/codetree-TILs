77978b184fce78bfcfa8b56fcec3b9825695c43a
Leeuc <92780253+Leewuc@users.noreply.github.com>
2024-04-08 14:24:32 +0900

    Title: 가장 짧은 문자열, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240408/가장 짧은 문자열/shortest-string.py
@@ -0,0 +1,18 @@
+n = input()
+n1 = input()
+n2 = input()
+nn = len(n)
+nn1 = len(n1)
+nn2 = len(n2)
+if nn > nn1 and nn1 > nn2:
+    print(nn-nn2)
+elif nn1 > nn and nn > nn2:
+    print(nn1-nn2)
+elif nn2 > nn and nn > nn1:
+    print(nn2-nn1)
+elif nn > nn2 and nn2 > nn1:
+    print(nn-nn1)
+elif nn1 > nn2 and nn2 > nn:
+    print(nn1-nn)
+else:
+    print(nn2-nn)
