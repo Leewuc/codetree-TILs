cd4fcf0eb565ad536b92cfb5bd989bb176e0e426
Leeuc <92780253+Leewuc@users.noreply.github.com>
2024-04-08 11:37:03 +0900

    Title: 지그재그로 숫자 채우기, Time: 136ms, Memory: 25MB, Status: Passed - CodeTree

--- 240408/지그재그로 숫자 채우기/zigzag-numbering.py
+++ 240408/지그재그로 숫자 채우기/zigzag-numbering.py
@@ -1,20 +1,27 @@
-def zigzag(n, m):
-    arr = [[0 for _ in range(m)] for _ in range(n)]
+# 변수 선언 및 입력
+n, m = tuple(map(int, input().split()))
+answer = [
+    [0 for _ in range(m)]
+    for _ in range(n)
+]
 
-    num = 0
-    for i in range(n):
-        if i % 2 == 0:
-            for j in range(m):
-                arr[i][j] = num
-                num += 1
-        else:
-            for j in range(m - 1, -1, -1):
-                arr[i][j] = num
-                num += 1
+count = 0
 
-    return arr
+# Step 1:
+for col in range(m):
+    if col % 2 == 0:
+        # Case 1:
+        for row in range(n):
+            answer[row][col] = count
+            count += 1
+    else:
+        # Case 2:
+        for row in range(n - 1, -1, -1):
+            answer[row][col] = count
+            count += 1
 
-n, m = 4, 2  # 지그재그 모양을 만들 크기 입력
-result = zigzag(n, m)
-for row in result:
-    print(' '.join(map(str, row)))
+# 출력:
+for row in range(n):
+    for col in range(m):
+        print(answer[row][col], end = ' ')
+    print()
