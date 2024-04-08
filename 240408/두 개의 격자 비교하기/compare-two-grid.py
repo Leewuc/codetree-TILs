40e3923e3104bbcd82c584de3ea0b3b823be3cac
Leeuc <92780253+Leewuc@users.noreply.github.com>
2024-04-08 10:54:37 +0900

    Title: 두 개의 격자 비교하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240408/두 개의 격자 비교하기/compare-two-grid.py
@@ -0,0 +1,19 @@
+a,b = map(int,input().split())
+n1=[]
+n2=[]
+arr_3 =[[0 for _ in range(a)] for _ in range(a)]
+for _ in range(a):
+    n1.append(list(map(int,input().split())))
+
+for _ in range(a):
+    n2.append(list(map(int,input().split())))
+for i in range(a):
+    for j in range(b):
+        if n1[i][j] == n2[i][j]:
+            arr_3[i][j] = 0
+        else:
+            arr_3[i][j] = 1
+for k in range(a):
+    for j in range(b):
+        print(arr_3[k][j],end=' ')
+    print()
