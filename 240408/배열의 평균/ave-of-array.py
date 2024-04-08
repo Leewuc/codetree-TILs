57627298b31dc0eb73f52b4683b42f603ab4aa60
Leeuc <92780253+Leewuc@users.noreply.github.com>
2024-04-08 09:48:48 +0900

    Title: 배열의 평균, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240408/배열의 평균/ave-of-array.py
@@ -0,0 +1,27 @@
+arr = [
+    list(map(int, input().split()))
+    for _ in range(2)
+]
+
+v = 0
+
+for i in range (2):
+    v = 0
+    for j in range(4):
+        v += arr[i][j]
+    print(f"{v/4:.1f}", end=" ")
+print()
+
+for i in range(4):
+    v = 0
+    for j in range(2):
+        v += arr[j][i]
+    print(f"{v/2:.1f}", end=" ")
+print()
+
+v = 0 
+
+for i in range (2):
+    for j in range(4):
+        v += arr[i][j]
+print(f"{v/8:.1f}")
