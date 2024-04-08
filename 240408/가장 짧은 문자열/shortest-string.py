319b82f22c3b16d0a4f5921c96c3588bb2b8dff1
Leeuc <92780253+Leewuc@users.noreply.github.com>
2024-04-08 14:27:51 +0900

    Title: 가장 짧은 문자열, Time: 128ms, Memory: 24MB, Status: Passed - CodeTree

--- 240408/가장 짧은 문자열/shortest-string.py
+++ 240408/가장 짧은 문자열/shortest-string.py
@@ -1,24 +1,12 @@
-n = input()
-n1 = input()
-n2 = input()
-nn = len(n)
-nn1 = len(n1)
-nn2 = len(n2)
-if nn == nn1:
-    nn = nn1
-elif nn == nn2:
-    nn = nn2
-elif nn1 == nn2:
-    nn1 = nn2
-if nn > nn1 and nn1 > nn2:
-    print(nn-nn2)
-elif nn1 > nn and nn > nn2:
-    print(nn1-nn2)
-elif nn2 > nn and nn > nn1:
-    print(nn2-nn1)
-elif nn > nn2 and nn2 > nn1:
-    print(nn-nn1)
-elif nn1 > nn2 and nn2 > nn:
-    print(nn1-nn)
-else:
-    print(nn2-nn)
+# 세 줄에 걸쳐 문자열 입력 받기
+string1 = input()
+string2 = input()
+string3 = input()
+
+# 문자열 길이 차이 계산
+min_length = min(len(string1), len(string2), len(string3))
+max_length = max(len(string1), len(string2), len(string3))
+difference = max_length - min_length
+
+# 결과 출력
+print(difference)
