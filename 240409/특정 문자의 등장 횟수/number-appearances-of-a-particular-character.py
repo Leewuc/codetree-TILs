ebd2f8fbd025dc41d1bbd9bbf2f24b8fff5c6e21
Leeuc <92780253+Leewuc@users.noreply.github.com>
2024-04-09 09:44:21 +0900

    Title: 특정 문자의 등장 횟수, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240409/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
+++ 240409/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
@@ -1,8 +1,16 @@
-n = input()
-cnt = 0
-cnt1 = 0
-if 'ee' in n:
-    cnt += 1
-if 'eb' in n:
-    cnt1 += 1
-print(cnt, cnt1)
+# 문자열을 입력 받습니다.
+string = input()
+
+# 'ee'와 'eb'의 개수를 세기 위한 변수를 초기화합니다.
+count_ee = 0
+count_eb = 0
+
+# 문자열을 순회하면서 'ee'와 'eb'의 개수를 세어줍니다.
+for i in range(len(string) - 1):
+    if string[i:i+2] == 'ee':
+        count_ee += 1
+    elif string[i:i+2] == 'eb':
+        count_eb += 1
+
+# 결과를 출력합니다.
+print(count_ee, count_eb)
