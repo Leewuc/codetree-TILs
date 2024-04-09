9d6ca0601f87f2b58b8121c134c9440f9e936670
Leeuc <92780253+Leewuc@users.noreply.github.com>
2024-04-09 15:52:03 +0900

    Title: 숫자로 이루어진 사각형, Time: 153ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240409/숫자로 이루어진 사각형/rectangle-with-a-number.py
@@ -0,0 +1,11 @@
+def squ(n):
+    cnt = 0
+    for i in range(n):
+        for j in range(n):
+            cnt += 1
+            if cnt > 9:
+                cnt = 1
+            print(cnt,end=" ")
+        print()
+n = int(input())
+squ(n)
