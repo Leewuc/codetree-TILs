d26a9b734b1f994a2c6873b703964ac70591049f
Leewoochang <92780253+Leewuc@users.noreply.github.com>
2024-04-15 16:00:59 +0900

    Title: 재귀함수를 이용한 피보나치 수, Time: 170ms, Memory: 26MB, Status: Passed - CodeTree

--- 240415/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.py
+++ 240415/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.py
@@ -1,10 +1,11 @@
-n = int(input())
-def pibo(m):
-    if n == 1:
-        return 1
-    if n == 2:
-        return 1
-    if n == 3:
-        return 2
-    return pibo(n-1) + pibo(n-2)
-print(pibo(n))
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+# 정수 N 입력 받기
+N = int(input())
+
+# N번째 피보나치 수 출력
+print(fibonacci(N))
