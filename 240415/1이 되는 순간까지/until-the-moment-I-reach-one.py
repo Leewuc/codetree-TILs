2827e5d5a84021c3c597c8d2c02cb39b67ea93cb
Leewoochang <92780253+Leewuc@users.noreply.github.com>
2024-04-15 15:53:00 +0900

    Title: 1이 되는 순간까지, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

--- 240415/1이 되는 순간까지/until-the-moment-I-reach-one.py
+++ 240415/1이 되는 순간까지/until-the-moment-I-reach-one.py
@@ -1,13 +1,16 @@
-def f(n):
-    cnt = 0
-    if n >= 1:
-        if n % 2 == 0:
-            n //= 2
-            cnt += 1
-            return cnt
-        else:
-            n //= 3
-            cnt += 1
-            return cnt
+# n 이 짝수이면 2 로 나누고
+# n 이 홀수이면 3 으로 나눈 몫
+# 값이 1이되면 모든 횟수를 구하는 작업
+
+
 n = int(input())
-print(f(n))
+
+def recursive(m):
+    if m == 1:
+        return 0
+    if m % 2 == 0:
+        return recursive(m // 2) + 1
+    else:
+        return recursive(m // 3) + 1
+
+print(recursive(n))
