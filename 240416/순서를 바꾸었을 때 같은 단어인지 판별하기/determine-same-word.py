fb2bc11358e3336989e985dfe095b9b5785a20d2
Leewoochang <92780253+Leewuc@users.noreply.github.com>
2024-04-16 09:31:21 +0900

    Title: 순서를 바꾸었을 때 같은 단어인지 판별하기, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240416/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.py
@@ -0,0 +1,16 @@
+n = input()
+n1 = input()
+n2 = list(n)
+n3 = list(n)
+n2.sort()
+n3.sort()
+boo = False
+for i in range(len(n)):
+    if n2[i] == n3[i]:
+        boo = True
+    else:
+        boo = False
+if boo == True:
+    print("Yes")
+else:
+    print("No")
