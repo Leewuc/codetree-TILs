173fb82487a3c79134765fee5d4205fece3eba12
Leewoochang <92780253+Leewuc@users.noreply.github.com>
2024-04-16 11:46:59 +0900

    Title: 사는 지역, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240416/사는 지역/where-live.py
@@ -0,0 +1,17 @@
+class local:
+    def __init__(self,name,number,loc):
+        self.name = name
+        self.number = number
+        self.loc = loc
+n = int(input())
+peop = []
+for _ in range(n):
+    x,y,z = tuple(input().split())
+    peop.append(local(x,y,z))
+ll = [person.name for person in peop]
+ll.sort(reverse=True)
+for i in range(n):
+    if peop[i].name == ll[n-1]:
+        print(f"name {peop[n-1].name}")
+        print(f"addr {peop[n-1].number}")
+        print(f"city {peop[n-1].loc}")
