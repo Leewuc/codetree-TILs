4c53ee3226ccb87c482b1f08dbb3f24ef973f461
Leewoochang <92780253+Leewuc@users.noreply.github.com>
2024-04-26 15:22:42 +0900

    Title: DateTime to DateTime, Time: 150ms, Memory: 24MB, Status: Passed - CodeTree

--- 240426/DateTime to DateTime/datetime-to-datetime.py
+++ 240426/DateTime to DateTime/datetime-to-datetime.py
@@ -1,7 +1,13 @@
-a,b,c = map(int,input().split())
-if a < 11 or b < 11 or c < 11:
+# 입력
+a, b, c = map(int, input().split())
+
+# 시작 시간과 종료 시간 간의 분 차이 계산
+start_time = 11 * 24 * 60 + 11 * 60 + 11  # 시작 시간을 분으로 변환
+end_time = a * 24 * 60 + b * 60 + c  # 종료 시간을 분으로 변환
+diff = end_time - start_time
+
+# 결과 출력
+if diff < 0:
     print(-1)
-d = (a - 11) * 1440
-d += ((b-11)*60)
-d += (c-11)
-print(d)
+else:
+    print(diff)
