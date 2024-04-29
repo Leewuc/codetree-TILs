de83799f757b08c2d5fd39f1d4616d1b7932a844
Leewoochang <92780253+Leewuc@users.noreply.github.com>
2024-04-29 09:30:57 +0900

    Title: 최대로 겹치는 지점, Time: 119ms, Memory: 25MB, Status: Passed - CodeTree

--- 240429/최대로 겹치는 지점/maximum-overlapped-points.py
+++ 240429/최대로 겹치는 지점/maximum-overlapped-points.py
@@ -1,14 +1,7 @@
+array = [0] * 100
 n = int(input())
-lines = []
-inter_line = {}
-cnt = 0
 for _ in range(n):
-    x1, x2 = map(int,input().split())
-    lines.append((x1,x2))
-for line in lines:
-    for i in range(line[0],line[1]):
-        if i in inter_line:
-            inter_line[i] += 1
-        else:
-            inter_line[i] = 1
-print(max(inter_line.values())+1)
+    x1, x2 = map(int, input().split())
+    for i in range(x1, x2+1):
+        array[i-1] += 1
+print(max(array))
