69ac76f5dbc0f4b0e71375cc4b695039dca1f247
Leewoochang <92780253+Leewuc@users.noreply.github.com>
2024-04-30 13:27:32 +0900

    Title: 벌금은 누구에게, Time: 189ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240430/벌금은 누구에게/who-will-pay.py
@@ -0,0 +1,10 @@
+n , m , k =map(int,input().split())
+num = []
+for i in range(m):
+    a = int(input())
+    num.append(a)
+    if num.count(a) == k:
+        print(a)
+        break
+else:
+    print(-1)
