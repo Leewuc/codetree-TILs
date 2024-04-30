c479aa0f78a7a53c3e353be53098923cd1cbedcf
Leewoochang <92780253+Leewuc@users.noreply.github.com>
2024-04-30 17:50:01 +0900

    Title: 1이 3개 이상 있는 위치, Time: 137ms, Memory: 26MB, Status: Passed - CodeTree

--- 240430/1이 3개 이상 있는 위치/place-more-than-3-ones.py
+++ 240430/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -1,17 +1,33 @@
+# 변수 선언 및 입력
 n = int(input())
-lst = [list(map(int,input().split())) for _ in range(n)]
-dx,dy = [0,1,0,-1],[1,0,-1,0]
-cnt = 0
-ans = 0
-def in_range(x,y):
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+dxs = [0, 1,  0, -1]
+dys = [1, 0, -1,  0]
+
+
+def in_range(x, y):
     return 0 <= x and x < n and 0 <= y and y < n
+
+
+def adjacent_cnt(x, y):
+    cnt = 0
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+        if in_range(nx, ny) and arr[nx][ny] == 1:
+            cnt += 1
+    
+    return cnt
+
+
+# 각 칸을 탐색합니다.
+ans = 0
 for i in range(n):
     for j in range(n):
-        for dxs,dys in zip(dx,dy):
-            nx,ny = i + dxs, j+ dys
-            if in_range(nx,ny) and lst[nx][ny] == 1:
-                cnt += 1
-    if cnt >= 3:
-        ans += 1    
-
+        if adjacent_cnt(i, j) >= 3:
+            ans += 1
+    
 print(ans)
