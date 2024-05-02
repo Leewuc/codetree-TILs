d608996eb822200ae432a469a50921718b4b5a85
Leewoochang <92780253+Leewuc@users.noreply.github.com>
2024-05-02 16:11:03 +0900

    Title: 되돌아오기, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240502/되돌아오기/come-back.py
@@ -0,0 +1,34 @@
+n = int(input())
+dirc = [list(map(str, input().split())) for _ in range(n)]
+
+count = 0
+total = 0
+
+for i in range(n):
+    dirc[i][1] = int(dirc[i][1])
+    total += dirc[i][1]
+
+def move(string, x, y):
+    if string == 'N':
+        return x-1, y
+    elif string == 'E':
+        return x, y+1
+    elif string == 'S':
+        return x+1, y
+    elif string == 'W':
+        return x, y-1
+
+n1, n2 = n, n
+
+a = 0
+for j in range(n):
+    for k in range(dirc[j][1]):
+        n1, n2 = move(dirc[j][0], n1, n2)
+        count += 1
+        a += 1
+        if n == n1 and n == n2:
+            print(count)
+            break   
+        elif a == total:
+            print(-1)
+            break
