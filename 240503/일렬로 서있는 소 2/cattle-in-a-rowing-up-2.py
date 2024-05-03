bbc5d94d6493fd883c3d647460f4c3a9408d2dc7
Leewoochang <92780253+Leewuc@users.noreply.github.com>
2024-05-03 11:16:02 +0900

    Title: 일렬로 서있는 소 2, Time: 115ms, Memory: 25MB, Status: Passed - CodeTree

--- 240503/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 240503/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -4,6 +4,6 @@ cnt = 0
 for i in range(n):
     for j in range(i+1,n):
         for k in range(j+1,n):
-            if a[i] < a[j] < a[k] and i < j < k:
+            if a[i] <= a[j] and a[j] <= a[k]:
                 cnt += 1
 print(cnt)
