a4d563c8a8a215ba5fb89d98edc1187a167f4c30
Leewoochang <92780253+Leewuc@users.noreply.github.com>
2024-05-03 09:31:31 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 156ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240503/빙빙 돌며 사각형 채우기/snail-alphabet-square.py
@@ -0,0 +1,44 @@
+n, m = tuple(map(int, input().split()))
+
+answer = [
+    [0] * m
+    for _ in range(n)
+]
+
+def in_range (x, y):
+
+    return 0 <= x < n and 0 <= y < m
+
+dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+
+x, y = 0, 0
+
+dir_num = 0
+
+answer[x][y] = 1
+
+for i in range(2, (n * m) + 1):
+
+    nx, ny = x + dxs[dir_num], y + dys[dir_num]
+
+    if not in_range(nx, ny) or answer[nx][ny] != 0:
+
+        dir_num = (dir_num + 1) % 4
+    
+    x, y = x + dxs[dir_num], y + dys[dir_num]
+
+    answer[x][y] = i
+
+
+for i in range(n):
+
+    for j in range(m):
+
+        if answer[i][j] > 26:
+
+            answer[i][j] %= 26
+            if answer[i][j] == 0:
+                answer[i][j] =='Z'
+        print(chr(answer[i][j] + 64), end = " ")
+
+    print()
