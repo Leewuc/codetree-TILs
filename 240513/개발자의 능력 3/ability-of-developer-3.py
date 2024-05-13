7963b50275269b7f5867f5b0382076ca77f8897c
Leewoochang <92780253+Leewuc@users.noreply.github.com>
2024-05-13 17:08:43 +0900

    Title: 개발자의 능력 3, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240513/개발자의 능력 3/ability-of-developer-3.py
+++ 240513/개발자의 능력 3/ability-of-developer-3.py
@@ -1,12 +1,13 @@
 n = list(map(int,input().split()))
 
-def get_diff(i,j):
-    sum1 = n[i] + n[j]
+def get_diff(i,j,k):
+    sum1 = n[i] + n[j] + n[k]
     sum2 = sum(n) - sum1
     return abs(sum1-sum2)
 
 min_diff = 100
 for i in range(0,6):
     for j in range(i+1,6):
-        min_diff = min(min_diff,get_diff(i,j))
+        for k in range(j+1,6):
+            min_diff = min(min_diff,get_diff(i,j,k))
 print(min_diff)
