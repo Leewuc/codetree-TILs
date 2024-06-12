a356d1ff96a1409a0dafd72b665d8aa94aebfb73
Leewoochang <92780253+Leewuc@users.noreply.github.com>
2024-06-12 14:17:10 +0900

    Title: 등차수열, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

--- 240612/등차수열/arithmetic-sequence.py
+++ 240612/등차수열/arithmetic-sequence.py
@@ -6,7 +6,7 @@ for k in range(1, 101):
     #두 수를 수열에서 뽑기
     cnt=0
     for i in range(n):
-        for j in range(i, n): #####이부분을 i+1로 바꾸어야 맞았다고 뜹니다#####
+        for j in range(i+1, n):
             if (arr[i]+arr[j])/2==k:
                 cnt+=1
     k_max=max(k_max, cnt)
